e1b4016d35db6dbce2f36e5c1c72c8802b470763
TenzDelek <tibetdelek@gmail.com>
2024-09-21 21:08:22 +0530

    framer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+"use client"
+import { motion, MotionValue, useSpring, useTransform } from "framer-motion"
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [timer,settimer]=useState(0)
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className=" flex items-center  justify-center gap-x-8 h-screen w-full">
+      <div className=" gap-x-2 flex-1 flex justify-end items-center">
+      time:{timer}
+      <input value={timer}
+      type="number"
+      min={0}
+      className=" text-black w-20 p-1 rounded-sm"
+      onChange={(e)=>settimer(+e.target.value)}
+      />
+      </div>
+      <div className=" flex-1  flex justify-start items-center">
+        <Counter value={timer} />
+      </div>
     </div>
   );
+}
+
+ const Counter = ({value}:{value:number}) => {
+  let animateval=useSpring(value)
+  useEffect(
+    () => {
+      animateval.set(value)
+    },[animateval,value]
+  )
+  return <div className=" flex overflow-hidden ring-2 h-6 ring-red-500">
+    
+    <div className=" w-6 relative">
+      {[...Array(10)].map((_,i) => //Array(10)makes an array of 10 elements with value 0
+        (<Number place={100} mv={animateval} number={i} key={i}/>))
+      }
+      </div><div className=" w-6 relative">
+      {[...Array(10)].map((_,i) => //Array(10)makes an array of 10 elements with value 0
+        (<Number place={10} mv={animateval} number={i} key={i}/>))
+      }
+      </div>
+      <div className=" w-6 relative">
+      {[...Array(10)].map((_,i) => //Array(10)makes an array of 10 elements with value 0
+        (<Number place={1} mv={animateval} number={i} key={i}/>))
+      }
+      </div>
+      </div>;
+}
+
+
+
+const Number = ({place,mv,number}:{place:number,mv:MotionValue,number:number})=>{
+let y=useTransform(mv,latest=>{
+ let height=24
+ let placevalue=(latest/place)%10
+ let offset=(10+number-placevalue)%10
+let memo= offset* height
+if(offset>5){
+  memo-=10*height
+}
+return memo
+})
+  return(
+  <motion.div style={{y}} className=" absolute inset-0 flex justify-center items-center">
+  {number}
+  </motion.div>
+)
 }
